326b87e6fcc0d67cb8ee8d8baaf2a0e14e7a84b1
jonas attia <jonas.attia@epita.fr>
2023-07-23 15:42:31 +0200

    (feat): header and nav_bar done

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  //When we click on each nav__link, we remove the show-menu
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((link_el) => link_el.addEventListener("click", linkAction));
