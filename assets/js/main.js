e50b8ce38d368f18737b59337a050e399fcd0052
jonas attia <jonas.attia@epita.fr>
2023-07-28 02:42:17 +0200

    (feat): scroll reveal animation done

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,3 +83,17 @@ const scrollUp = () => {
     : scrollUp.classList.remove("show-scroll");
 };
 window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.home__data, .home__social, .contact__container, footer__container`);
+sr.reveal(`.home__image`, {origin: 'bottom'});
+sr.reveal(`.about__data, .skills_data`, {origin: 'left'});
+sr.reveal(`.about__image, .skills_content`, {origin: 'right'});
+sr.reveal(`.card`, {interval: 100});
